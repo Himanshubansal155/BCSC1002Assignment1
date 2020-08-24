736df25397e8a99671b4f8f5f968a54aaec9e6cf
Himanshu Bansal <himanshubansal155@gmail.com>
2020-08-24 21:25:35 +0530

    creates getter and setter method for student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -8,6 +8,30 @@ public class Student {
     private String nameOfStudent;
     private long universityRollNumber;
     private int noOfBookIssued;
-    private Book[] book;
-    
+    private Book[] booksStore;
+
+    public String getNameOfStudent() {
+        return nameOfStudent;
+    }
+    public void setNameOfStudent(String nameOfStudent) {
+        this.nameOfStudent = nameOfStudent;
+    }
+    public long getUniversityRollNumber() {
+        return universityRollNumber;
+    }
+    public void setUniversityRollNumber(long universityRollNumber) {
+        this.universityRollNumber = universityRollNumber;
+    }
+    public int getNoOfBookIssued() {
+        return noOfBookIssued;
+    }
+    public void setNoOfBookIssued(int noOfBookIssued) {
+        this.noOfBookIssued = noOfBookIssued;
+    }
+    public Book[] getBooksStore() {
+        return booksStore;
+    }
+    public void setBooksStore(Book[] booksStore) {
+        this.booksStore = booksStore;
+    }
 }
