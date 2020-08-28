df57cd3c9cb22b0fb10f019988ba54841cc5a739
Himanshu Bansal <himanshubansal155@gmail.com>
2020-08-28 10:04:22 +0530

    Create to String method for student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -4,6 +4,8 @@
 
 package definitions;
 
+import java.util.Arrays;
+
 public class Student {
     private String nameOfStudent;
     private long universityRollNumber;
@@ -13,25 +15,38 @@ public class Student {
     public String getNameOfStudent() {
         return nameOfStudent;
     }
+
     public void setNameOfStudent(String nameOfStudent) {
         this.nameOfStudent = nameOfStudent;
     }
+
     public long getUniversityRollNumber() {
         return universityRollNumber;
     }
+
     public void setUniversityRollNumber(long universityRollNumber) {
         this.universityRollNumber = universityRollNumber;
     }
+
     public int getNoOfBookIssued() {
         return noOfBookIssued;
     }
+
     public void setNoOfBookIssued(int noOfBookIssued) {
         this.noOfBookIssued = noOfBookIssued;
     }
-    public Book[] getBooksStore() {
+
+    public Book[] getBooksStore() {k
         return booksStore.clone();
     }
+
     public void setBooksStore(Book[] booksStore) {
         this.booksStore = booksStore;
+    }
+
+    @Override
+    public String toString() {
+        return "Student Name: " + nameOfStudent + "University RollNumber: " + universityRollNumber
+                + "No. of Books Issued: " + noOfBookIssued + "Issued Books: " + Arrays.toString(booksStore);
     }
 }
