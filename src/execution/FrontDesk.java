6ffc813d52d9d7fd23414f121b6cd612f4f28023
Himanshu Bansal <himanshubansal155@gmail.com>
2020-08-28 17:43:14 +0530

    Creates A Menu for frontdesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -4,11 +4,26 @@
 
 package execution;
 
-import definitions.Library;
+import java.util.Scanner;
 
 public class FrontDesk {
+    private static final int ISSUE_BOOK = 1;
+    private static final int RETURN_BOOK = 2;
+    private static final int SHOW_ALL_BOOKS = 3;
+    private static final int EXIT = 4;
+
     public static void main(String[] args) {
-        Library library = new Library();
-        library.getBooksAvailable();
+        Scanner scanner = new Scanner(System.in);
+        int customerInput;
+        do {
+            System.out.print("-=-=--=-=-");
+            System.out.print("Welcome To The Front Desk");
+            System.out.println("-=-=--=-=-");
+            System.out.println("How may I help you today?");
+            System.out.println("1. Issue a new book for me.");
+            System.out.println("2. Return a previously issues book for me.");
+            System.out.println("3. Show me all my issues books.");
+            System.out.println("4. Exit.");
+        } while (customerInput != EXIT);
     }
 }
